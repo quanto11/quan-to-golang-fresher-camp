fc6266fcbe40fb0f3222f4ec0e3929ee5c982cb0
tohongquan <tohongquan2000@gmail.com>
2022-01-25 22:46:25 +0700

    make color

--- main.go
+++ main.go
@@ -10,17 +10,17 @@ import (
 )
 
 type User struct {
-	User_id          int    `json:"user_id,omitempty" gorm:"column:user_id"`
-	User_firstname   string `json:"user_firstname" gorm:"column:user_firstname"`
-	User_lastname    string `json:"user_lastname" gorm:"column:user_lastname"`
-	User_phonenumber string `json:"user_phonenumber" gorm:"column:user_phonenumber"`
-	Username         string `json:"username" gorm:"column:username"`
-	Password         string `json:"password" gorm:"column:password"`
+	UserId          int    `json:"user_id,omitempty" gorm:"column:user_id"`
+	UserFirstname   string `json:"user_firstname" gorm:"column:user_firstname"`
+	UserLastname    string `json:"user_lastname" gorm:"column:user_lastname"`
+	UserPhonenumber string `json:"user_phonenumber" gorm:"column:user_phonenumber"`
+	Username        string `json:"username" gorm:"column:username"`
+	Password        string `json:"password" gorm:"column:password"`
 }
 
 type UserUpdate struct {
-	User_firstname *string `json:"user_firstname" gorm:"column:user_firstname"`
-	User_lastname  *string `json:"user_lastname" gorm:"column:user_lastname"`
+	UserFirstname *string `json:"user_firstname" gorm:"column:user_firstname"`
+	UserLastname  *string `json:"user_lastname" gorm:"column:user_lastname"`
 }
 
 func (User) TableName() string {
@@ -34,7 +34,7 @@ func main() {
 		log.Fatal(err)
 	}
 	// INSERT
-	newUser := User{User_firstname: "To", User_lastname: "Quan", User_phonenumber: "0962xxx166", Username: "tohongquan", Password: "abc123"}
+	newUser := User{UserFirstname: "To", UserLastname: "Quan", UserPhonenumber: "0962xxx166", Username: "tohongquan", Password: "abc123"}
 	if err := db.Create(&newUser); err != nil {
 		fmt.Println(err)
 	}
@@ -43,18 +43,21 @@ func main() {
 	//SELECT list user
 	var users []User
 	db.Where("status=?", 1).Find(&users)
-	fmt.Println("List users:", users)
+	fmt.Println("List users:")
+	for _, i := range users {
+		fmt.Printf("\tId:%v\tFirstname:%v\tLastname:%v\tPhonenumber:%v\tUsername:%v\tPassword:%v\n", i.UserId, i.UserFirstname, i.UserLastname, i.UserPhonenumber, i.Username, i.Password)
+	}
+
 	//SELECT a user
 	var user User
-	if err := db.Where("user_id = 1").First(&user); err != nil {
+	if err := db.Where("user_id = 1").First(&user); err == nil {
 		fmt.Println(err)
 	}
-	fmt.Println("user:", user)
-
+	fmt.Printf("User\t:\tId:%v\tFirstname:%v\tLastname:%v\tPhonenumber:%v\tUsername:%v\tPassword:%v\n", user.UserId, user.UserFirstname, user.UserLastname, user.UserPhonenumber, user.Username, user.Password)
 	//UPDATE
 	var updateUser User
-	updateUser.User_firstname = "Hong"
-	updateUser.User_lastname = "Quan"
+	updateUser.UserFirstname = "Hong"
+	updateUser.UserLastname = "Quan"
 	db.Table(User{}.TableName()).Where("user_id = 5").Updates(&updateUser)
 	//possible UPDATE EMPTY value
 	emptyStr := ""
