9eb6cb50ba1ce79ce67ad5d080d9ab3daff2c27b
tohongquan <tohongquan2000@gmail.com>
2022-01-26 16:54:53 +0700

    fix bug

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 )
 
 type User struct {
-	UserId          int    `json:"user_id,omitempty" gorm:"column:user_id;"`
+	UserId          int    `json:"user_id,omitempty" gorm:"column:id;"`
 	UserFirstname   string `json:"user_firstname" gorm:"column:user_firstname;"`
 	UserLastname    string `json:"user_lastname" gorm:"column:user_lastname;"`
 	UserPhonenumber string `json:"user_phonenumber" gorm:"column:user_phonenumber;"`
@@ -35,34 +35,39 @@ func main() {
 	}
 	// INSERT
 	newUser := User{UserFirstname: "To", UserLastname: "Quan", UserPhonenumber: "0962xxx166", Username: "tohongquan", Password: "abc123"}
-	if err := db.Create(&newUser); err != nil {
-		log.Println(err)
+	if err := db.Create(&newUser).Error; err != nil {
+		fmt.Println(err)
 	}
 	fmt.Println("New User:", newUser)
-	// SELECT
+	//SELECT
 	//SELECT list user
-	//var users []User
-	//db.Where("status=?", 1).Find(&users)
-	//fmt.Println("List users:")
-	//for _, i := range users {
-	//	fmt.Printf("\tId:%v\tFirstname:%v\tLastname:%v\tPhonenumber:%v\tUsername:%v\tPassword:%v\n", i.UserId, i.UserFirstname, i.UserLastname, i.UserPhonenumber, i.Username, i.Password)
-	//}
-	//
-	////SELECT a user
-	//var user User
-	//if err := db.Where("user_id = 1").First(&user); err != nil {
-	//	log.Println(err)
-	//}
-	//fmt.Printf("User\t:\tId:%v\tFirstname:%v\tLastname:%v\tPhonenumber:%v\tUsername:%v\tPassword:%v\n", user.UserId, user.UserFirstname, user.UserLastname, user.UserPhonenumber, user.Username, user.Password)
-	////UPDATE
-	//var updateUser User
-	//updateUser.UserFirstname = "Hong"
-	//updateUser.UserLastname = "Quan"
-	//db.Table(User{}.TableName()).Where("user_id = 5").Updates(&updateUser)
-	////possible UPDATE EMPTY value
-	//emptyStr := ""
-	//db.Table(User{}.TableName()).Where("user_id = 4").Updates(UserUpdate{&emptyStr, &emptyStr})
-	//
-	////DELETE
-	////db.Table(User{}.TableName()).Where("user_id = 6").Delete(nil)
+	var users []User
+	db.Where("status=?", 1).Find(&users)
+	fmt.Println("List users:")
+	for _, i := range users {
+		fmt.Printf("\tId:%v\tFirstname:%v\tLastname:%v\tPhonenumber:%v\tUsername:%v\tPassword:%v\n", i.UserId, i.UserFirstname, i.UserLastname, i.UserPhonenumber, i.Username, i.Password)
+	}
+
+	//SELECT a user
+	var user User
+	if err := db.Where("id = 1").First(&user).Error; err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("User\t:\tId:%v\tFirstname:%v\tLastname:%v\tPhonenumber:%v\tUsername:%v\tPassword:%v\n", user.UserId, user.UserFirstname, user.UserLastname, user.UserPhonenumber, user.Username, user.Password)
+
+	//UPDATE
+	var updateUser User
+	updateUser.UserFirstname = "Hong"
+	updateUser.UserLastname = "Quan"
+	if err := db.Table(User{}.TableName()).Where("id = 5").Updates(&updateUser).Error; err != nil {
+		fmt.Println(err)
+	}
+
+	//possible UPDATE EMPTY value
+	firstname := ""
+	lastname := "Quan"
+	db.Table(User{}.TableName()).Where("id = 13").Updates(UserUpdate{&firstname, &lastname})
+
+	//DELETE
+	db.Table(User{}.TableName()).Where("id = 6").Delete(nil)
 }
