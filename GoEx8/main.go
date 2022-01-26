50f4ecfec23dc0cb7e3b218dfd3910623848f7f1
tohongquan <tohongquan2000@gmail.com>
2022-01-26 18:26:22 +0700

    add GoEx8

---
+++ GoEx8/main.go
@@ -0,0 +1,188 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+type User struct {
+	UserId          int    `json:"id,omitempty" gorm:"column:id;"`
+	UserFirstname   string `json:"user_firstname" gorm:"column:user_firstname;"`
+	UserLastname    string `json:"user_lastname" gorm:"column:user_lastname;"`
+	UserPhonenumber string `json:"user_phonenumber" gorm:"column:user_phonenumber;"`
+	Username        string `json:"username" gorm:"column:username;"`
+	Password        string `json:"password" gorm:"column:password;"`
+}
+
+type UserUpdate struct {
+	UserFirstname   *string `json:"user_firstname" gorm:"column:user_firstname"`
+	UserLastname    *string `json:"user_lastname" gorm:"column:user_lastname"`
+	UserPhonenumber *string `json:"user_phonenumber" gorm:"column:user_phonenumber;"`
+	Username        *string `json:"username" gorm:"column:username;"`
+	Password        *string `json:"password" gorm:"column:password;"`
+}
+
+func (User) TableName() string {
+	return "users"
+}
+
+func (UserUpdate) TableName() string {
+	return User{}.TableName()
+}
+
+func main() {
+
+	dsn := os.Getenv("DBConnection")
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	//CRUD
+
+	user := r.Group("/users")
+	{
+		user.POST("", func(c *gin.Context) {
+			var data User
+
+			if err := c.ShouldBind(&data); err != nil {
+				c.JSON(401, gin.H{
+					"error": err.Error(),
+				})
+
+				return
+			}
+
+			if err := db.Create(&data).Error; err != nil {
+				c.JSON(401, gin.H{
+					"error": err.Error(),
+				})
+
+				return
+			}
+
+			c.JSON(http.StatusOK, data)
+		})
+
+		user.GET("/:id", func(c *gin.Context) {
+			id, err := strconv.Atoi(c.Param("id"))
+
+			if err != nil {
+				c.JSON(401, gin.H{
+					"error": err.Error(),
+				})
+
+				return
+			}
+
+			var data User
+
+			if err := db.Where("id = ?", id).First(&data).Error; err != nil {
+				c.JSON(401, gin.H{
+					"error": err.Error(),
+				})
+
+				return
+			}
+
+			c.JSON(http.StatusOK, data)
+		})
+
+		user.GET("", func(c *gin.Context) {
+			var data []User
+
+			type Filter struct {
+				Status int8 `json:"status" gorm:"column:status;"`
+			}
+
+			var filter Filter
+
+			c.ShouldBind(&filter)
+
+			newDb := db
+
+			if filter.Status == 0 {
+				newDb = db.Where("status = ?", filter.Status)
+			}
+
+			if err := newDb.Find(&data).Error; err != nil {
+				c.JSON(401, gin.H{
+					"error": err.Error(),
+				})
+
+				return
+			}
+
+			c.JSON(http.StatusOK, data)
+		})
+
+		user.PATCH("/:id", func(c *gin.Context) {
+			id, err := strconv.Atoi(c.Param("id"))
+
+			if err != nil {
+				c.JSON(401, map[string]interface{}{
+					"error": err.Error(),
+				})
+
+				return
+			}
+
+			var data UserUpdate
+
+			if err := c.ShouldBind(&data); err != nil {
+				c.JSON(401, map[string]interface{}{
+					"error": err.Error(),
+				})
+
+				return
+			}
+
+			if err := db.Where("id = ?", id).Updates(data).Error; err != nil {
+				c.JSON(401, map[string]interface{}{
+					"error": "Update failed",
+				})
+
+				return
+			}
+
+			c.JSON(http.StatusOK, map[string]interface{}{
+				"update successfully": 1,
+			})
+		})
+
+		user.DELETE("/:id", func(c *gin.Context) {
+			id, err := strconv.Atoi(c.Param("id"))
+
+			if err != nil {
+				c.JSON(401, map[string]interface{}{
+					"error": err.Error(),
+				})
+
+				return
+			}
+
+			if err := db.Table(User{}.TableName()).Where("id = ?", id).Delete(nil).Error; err != nil {
+				c.JSON(401, map[string]interface{}{
+					"error": err.Error(),
+				})
+
+				return
+			}
+
+			c.JSON(200, map[string]interface{}{"ok": 1})
+		})
+	}
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
